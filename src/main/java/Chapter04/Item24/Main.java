b414aaafe878d87a0597cabfa703687ded431a7e
heyazoo1007 <heyazoo1007@gmail.com>
2023-12-24 21:25:31 +0900

    2023-12-24 study : 이펙티브 자바 공부 기록, 아이템24 : 멤버 클래스는 되도록 static으로 만들라

---
+++ src/main/java/Chapter04/Item24/Main.java
@@ -0,0 +1,11 @@
+package Chapter04.Item24;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        outer.B();
+        outer.C();
+    }
+}
