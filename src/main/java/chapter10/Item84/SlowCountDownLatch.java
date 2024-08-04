60dd551a1f2b0478c32afe3a4dd0b494669e332d
heyazoo1007 <heyazoo1007@gmail.com>
2024-08-04 19:12:41 +0900

    2024-08-04 아이템84: 프로그램의 동작을 스레드 스케줄러에 기대지 말라

---
+++ src/main/java/chapter10/Item84/SlowCountDownLatch.java
@@ -0,0 +1,31 @@
+package chapter10.Item84;
+
+// 아이템84 : 프로그램의 동작을 스레드 스케줄러에 기대지 말라
+
+// BAD CASE : 바쁜 대기 상태 예시
+public class SlowCountDownLatch {
+    private int count;
+
+    public SlowCountDownLatch(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException(count + " < 0");
+        }
+        this.count = count;
+    }
+
+    public void await() {
+        while (true) {
+            synchronized (this) {
+                if (count == 0) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public synchronized void countDown() {
+        if (count != 0) {
+            count--;
+        }
+    }
+}
